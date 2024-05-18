564c55b3dc3ee741ffbaa54c9558c18d4f9ca499
DigitalDreamsTechnology <cardenasedwin1609@gmail.com>
2024-05-17 22:36:23 -0500

    Add files via upload

---
+++ assets/js/scripts.js
@@ -0,0 +1,15 @@
+document.getElementById("photoshoplink").addEventListener("click", function() {
+    window.open("https://go.hotmart.com/F91236347Y?dp=1", "_blank"); // Cambia "otra_pagina.html" por la URL de la página a la que deseas redirigir
+});
+document.getElementById("inteligencialink").addEventListener("click", function() {
+    window.open("https://go.hotmart.com/H91188387N", "_blank"); // Cambia "otra_pagina.html" por la URL de la página a la que deseas redirigir
+});
+document.getElementById("fotografialink").addEventListener("click", function() {
+    window.open("https://go.hotmart.com/Q91357388X?dp=1 ", "_blank"); // Cambia "otra_pagina.html" por la URL de la página a la que deseas redirigir
+});
+document.getElementById("googlelink").addEventListener("click", function() {
+    window.open("https://go.hotmart.com/A91234007O?dp=1 ", "_blank"); // Cambia "otra_pagina.html" por la URL de la página a la que deseas redirigir
+});
+document.getElementById("nexuslink").addEventListener("click", function() {
+    window.open("https://go.hotmart.com/A91234007O?dp=1 ", "_blank"); // Cambia "otra_pagina.html" por la URL de la página a la que deseas redirigir
+});
